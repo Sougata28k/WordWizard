29473912926b866749ac5da4e93997d0d66b7381
Sougata Karmakar <86069522+Sougata28k@users.noreply.github.com>
2023-11-19 12:20:36 +0530

    Add files via upload
    Added some more logic to the website like one can know how many characters and words are written and some more functions like lowercase, titlecase etc. The color mode also can be changed to dark

--- TextForm.js
+++ TextForm.js
@@ -1,18 +1,47 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
+    
+        const handleHover = () => {
+          console.log('Click to capitalize the first word of each sentence!'); // You can replace this with your own logic
+        };
+        const handleHover2 = () => {
+            console.log('Click to capitalize the first word of each word!'); // You can replace this with your own logic
+          };
+          const handleHover3 = () => {
+            console.log('Click to upper case the words!'); // You can replace this with your own logic
+          };
+          const handleHover4 = () => {
+            console.log('Click to lower case the words!'); // You can replace this with your own logic
+          };
+    const capitalizeEachSentence = (str) => {
+        return str
+          .split(/\.\s+/) // Split the text into an array of sentences
+          .map((sentence) => sentence.charAt(0).toUpperCase() + sentence.slice(1))
+          .join(". "); // Join the sentences back into a string
+      };
+      const handleOnClick5=()=>{
+        let newText = capitalizeEachSentence(text);
+        setTextHistory([...textHistory, text]);
+        setText(newText);
+       
+    }
     const handleOnClick=()=>{
         let newText = text.toUpperCase();
+        setTextHistory([...textHistory, text]);
         setText(newText);
        
     }
     const handleOnClick3 = () => {
         let newText = '';
+        setTextHistory([...textHistory, text]);
         setText(newText);
       };
+     
       
     const handleOnClick2=()=>{
         let newText = text.toLowerCase();
+        setTextHistory([...textHistory, text]);
         setText(newText);
     }
     const CapitalizeFirstLetter = (str) => {
@@ -23,27 +52,85 @@ export default function TextForm(props) {
       };
       const handleOnClick4 =()=>{
         let newText = CapitalizeFirstLetter(text);
+        setTextHistory([...textHistory, text]);
         setText(newText);
+        
       }
     const handleOnChange =(event)=>{
         setText(event.target.value);
     }
+    const handleUndo =()=>{
+        if (textHistory.length > 0) {
+            const previousText = textHistory.pop();
+            setText(previousText);
+            setTextHistory([...textHistory]);
+          }
+    }
+    const [myStyle, setMystyle] = useState({
+      color: 'black',
+      backgroundColor: 'white',
+    });
+    const [btnText, setbtnText] = useState("Enable Dark Mode"
+    );
+    
+    const modeChange = () => {
+      if (myStyle.color === 'black') {
+        setMystyle({
+          color: 'white',
+          backgroundColor: 'black',
+        });
+        setbtnText("Disable Dark Mode");
+      } else {
+        setMystyle({
+          color: 'black',
+          backgroundColor: 'white',
+        });
+        setbtnText("Enable Dark Mode");
+      }
+    };
+    
+
+
   const [text, setText] = useState("Enter your text here");
+  const [textHistory, setTextHistory] = useState([]);
   return (
-    <div>
+    <div style={myStyle}>
+    <div className="container ">
+    
       <h1> {props.title}</h1>
-      <div class="mb-3">
-        <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text}  rows="8"></textarea>
+      
+      <div className="mb-3 my-3" >
+        <textarea className="form-control " style={myStyle} id="myBox" onChange={handleOnChange} value={text}  rows="12"></textarea>
       </div>
       <div className="container my-3 ">
-      <button className="btn btn-primary" onClick={handleOnClick} >Click to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleOnClick} onMouseEnter={handleHover3}
+      title="Click to upper case the words!" >Uppercase</button>
       
-      <button className="btn btn-primary mx-3"  onClick={handleOnClick2} >Click to Lowercase</button>
-      <button className="btn btn-primary "  onClick={handleOnClick4} >Click to Capitalize First Letter</button>   
+      <button className="btn btn-primary mx-3"  onClick={handleOnClick2} onMouseEnter={handleHover4}
+      title="Click to lower case the words!">Lowercase</button>
+      <button className="btn btn-primary "  onClick={handleOnClick4} onMouseEnter={handleHover2}
+      title="Click to capitalize the first letter of each word!">Title Case</button>  
+      <button className="btn btn-primary mx-3"  onClick={handleOnClick5} onMouseEnter={handleHover}
+      title="Click to capitalize the first word of each sentence!">Sentence Case</button>
+      <button className="btn btn-secondary " onClick={handleUndo}>
+          Undo
+        </button>    
       <button className="btn btn-danger mx-3 "  onClick={handleOnClick3} >Clear</button>
+      
+      {/* <div className="container my-3"> */}
+
+      <h3 className="my-3">Text Summary</h3>
+       <p> {text.split(" ").length} words and {text.length} characters </p>
+       {/* </div> */}
       
       
       </div>
+      <div className="container">
+    <button onClick={modeChange} type="button" className="btn btn-secondary my-3">{btnText}</button>
     </div>
+    </div>
+    
+    </div>
+    
   );
 }
